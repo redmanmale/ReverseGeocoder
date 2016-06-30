31bca68232b3645800883cb26b9cc95ef2f4e922
Jason Rai <jason.c.rai@gmail.com>
2016-06-30 09:44:23 -0400

    added sample

---
+++ ConsoleSample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GeoSharp.ReverseGeoCode geocode = new GeoSharp.ReverseGeoCode(@"C:\\geodata\cities1000.txt", true);
+            var name = geocode.NearestPlace(43.6442334, -79.3725049);
+            Console.WriteLine(name.Name);
+            Console.ReadLine();
+        }
+    }
+}
