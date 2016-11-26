8ebbc018dc72cc9e4e8614f11a66da5d8dd9f81d
Gerald Degeneve <gerald.degeneve@gmail.com>
2016-11-26 11:34:29 +0100

    Added more output to sample.

--- ConsoleSample/Program.cs
+++ ConsoleSample/Program.cs
@@ -1,18 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ConsoleSample
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            GeoSharp.ReverseGeoCode geocode = new GeoSharp.ReverseGeoCode(@"C:\\geodata\cities1000.txt", true);
-            var name = geocode.NearestPlace(43.6442334, -79.3725049);
-            Console.WriteLine(name.Name);
+            GeoSharp.ReverseGeoCode geocode = new GeoSharp.ReverseGeoCode(@"C:\\geodata\cities5000.txt");
+            var place = geocode.NearestPlace(48.1158333, 16.5643811);
+            Console.WriteLine("Name: " + place.Name);
+            Console.WriteLine("Feature Class: " + place.FeatureClass);
+            Console.WriteLine("Country Code: " + place.CountryCode);
+
             Console.ReadLine();
         }
     }
